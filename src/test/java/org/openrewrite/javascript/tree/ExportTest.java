06366e7e54aff2a976b9b32fc394e3bf75a2a53a
traceyyoshima <tracey.yoshima@gmail.com>
2023-05-08 15:50:14 -0600

    Add tests for export.

--- src/test/java/org/openrewrite/javascript/tree/ExportTest.java
+++ src/test/java/org/openrewrite/javascript/tree/ExportTest.java
@@ -1,4 +1,63 @@
 package org.openrewrite.javascript.tree;
 
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ExpectedToFail;
+
+@SuppressWarnings({"JSFileReferences", "JSUnusedLocalSymbols"})
 public class ExportTest extends ParserTest {
+
+    @ExpectedToFail
+    @Test
+    void exportDeclaration() {
+        rewriteRun(
+          javascript(
+            """
+              class ZipCodeValidator {
+                isAcceptable(s: string) {
+                  return s.length === 5;
+                }
+              }
+              export { ZipCodeValidator };
+              """
+          )
+        );
+    }
+
+    @ExpectedToFail
+    @Test
+    void propertyModifier() {
+        rewriteRun(
+            javascript(
+              """
+                export const numberRegexp = /^[0-9]+$/;
+                """
+            )
+        );
+    }
+
+    @ExpectedToFail
+    @Test
+    void fromClass() {
+        rewriteRun(
+          javascript("class CreateFile {}"),
+          javascript(
+            """
+              export * from "./f0.ts";
+              """
+          )
+        );
+    }
+
+    @ExpectedToFail
+    @Test
+    void alias() {
+        rewriteRun(
+          javascript("class CreateFile {}"),
+          javascript(
+            """
+              export * from "./f0.ts";
+              """
+          )
+        );
+    }
 }
