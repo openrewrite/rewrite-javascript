c1b54fd0c21059d5bf9609e94cb32c773218f641
traceyyoshima <tracey.yoshima@gmail.com>
2023-05-12 11:52:57 -0600

    Added licenses.

--- src/main/java/org/openrewrite/javascript/internal/tsc/package-info.java
+++ src/main/java/org/openrewrite/javascript/internal/tsc/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2022 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.javascript.tree;
+@NonNullApi
+@NonNullFields
+package org.openrewrite.javascript.internal.tsc;
 
-public class ImportTest extends ParserTest {
-}
+import org.openrewrite.internal.lang.NonNullApi;
+import org.openrewrite.internal.lang.NonNullFields;
