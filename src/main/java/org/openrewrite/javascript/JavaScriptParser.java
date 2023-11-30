de892418a56632b06410927e4f26eb2352903da3
traceyyoshima <tracey.yoshima@gmail.com>
2023-11-30 16:23:00 -0700

    Handle RunTime exception thrown in TSCRuntime init.

--- src/main/java/org/openrewrite/javascript/JavaScriptParser.java
+++ src/main/java/org/openrewrite/javascript/JavaScriptParser.java
@@ -24,7 +24,6 @@ import org.openrewrite.internal.EncodingDetectingInputStream;
 import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.internal.JavaTypeCache;
 import org.openrewrite.javascript.internal.JavetNativeBridge;
-import org.openrewrite.javascript.internal.TsTreePrinter;
 import org.openrewrite.javascript.internal.TypeScriptParserVisitor;
 import org.openrewrite.javascript.internal.tsc.TSCRuntime;
 import org.openrewrite.javascript.tree.JS;
@@ -41,7 +40,6 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
 
-@SuppressWarnings("ConstantValue")
 public class JavaScriptParser implements Parser {
 
     @Nullable
@@ -118,34 +116,38 @@ public class JavaScriptParser implements Parser {
             sourceTextsForTSC.put(relativePath, sourceText.sourceText);
         });
 
-        //noinspection resource
-        runtime().parseSourceTexts(
-                sourceTextsForTSC,
-                (node, context) -> {
-                    SourceWrapper source = sourcesByRelativePath.get(context.getRelativeSourcePath());
+        try {
+            //noinspection resource
+            runtime().parseSourceTexts(
+                    sourceTextsForTSC,
+                    (node, context) -> {
+                        SourceWrapper source = sourcesByRelativePath.get(context.getRelativeSourcePath());
 
-                    parsingListener.startedParsing(source.getInput());
-                    TypeScriptParserVisitor fileMapper = new TypeScriptParserVisitor(
-                            node,
-                            context,
-                            source.getSourcePath(),
-                            new JavaTypeCache(),
-                            source.getCharset().toString(),
-                            source.isCharsetBomMarked(),
-                            styles
-                    );
-                    SourceFile cu;
-                    try {
-                        cu = fileMapper.visitSourceFile();
-                        parsingListener.parsed(source.getInput(), cu);
-                    } catch (Throwable t) {
-                        ((ExecutionContext) pctx).getOnError().accept(t);
-                        cu = ParseError.build(JavaScriptParser.builder().build(), source.getInput(), relativeTo, pctx, t);
+                        parsingListener.startedParsing(source.getInput());
+                        TypeScriptParserVisitor fileMapper = new TypeScriptParserVisitor(
+                                node,
+                                context,
+                                source.getSourcePath(),
+                                new JavaTypeCache(),
+                                source.getCharset().toString(),
+                                source.isCharsetBomMarked(),
+                                styles
+                        );
+                        SourceFile cu;
+                        try {
+                            cu = fileMapper.visitSourceFile();
+                            parsingListener.parsed(source.getInput(), cu);
+                        } catch (Throwable t) {
+                            ((ExecutionContext) pctx).getOnError().accept(t);
+                            cu = ParseError.build(JavaScriptParser.builder().build(), source.getInput(), relativeTo, pctx, t);
+                        }
+
+                        compilationUnits.add(cu);
                     }
-
-                    compilationUnits.add(cu);
-                }
-        );
+            );
+        } catch (Exception e) {
+            return acceptedInputs(sources).map(input -> ParseError.build(this, input, relativeTo, ctx, e));
+        }
         return compilationUnits.stream();
     }
 
