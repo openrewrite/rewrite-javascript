a7edf2eccfcb46862b9717607b38a56589060921
Knut Wannheden <knut@moderne.io>
2024-09-12 13:02:11 +0200

    Initial implementation of `getJavaType()` and `withJavaType()`

--- openrewrite/src/javascript/extensions.ts
+++ openrewrite/src/javascript/extensions.ts
@@ -1,7 +1,12 @@
 import {J, JavaType} from "../java";
+import * as java_extensions from "../java/extensions";
 
 export function getJavaType<T extends J>(expr: T): JavaType | null {
+    // FIXME implement for JS types
+    return java_extensions.getJavaType(expr);
 }
 
 export function withJavaType<T>(expr: T, type: JavaType): T {
+    // FIXME implement for JS types
+    return java_extensions.withJavaType(expr, type);
 }
