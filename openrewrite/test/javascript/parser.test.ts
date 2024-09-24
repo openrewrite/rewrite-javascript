fbc42e65c11fb48a5e2735e45c6ee9bfbf20054f
Knut Wannheden <knut@moderne.io>
2024-09-24 14:45:02 +0200

    Make parser tests a bit nicer

--- openrewrite/test/javascript/parser.test.ts
+++ openrewrite/test/javascript/parser.test.ts
@@ -2,23 +2,54 @@ import {InMemoryExecutionContext, ParserInput} from '../../src/core';
 import {JavaScriptParser} from "../../src/javascript";
 import * as J from "../../src/java/tree";
 import * as JS from "../../src/javascript/tree";
+import dedent from "dedent";
 
-describe('JavaScriptParser', () => {
+describe('Parser API', () => {
+    const parser = JavaScriptParser.builder().build();
+
     test('parseInputs', () => {
-        const parser = JavaScriptParser.builder().build();
-        const [sourceFile] = parser.parseInputs([new ParserInput('foo.ts', null, true, () => Buffer.from('1', 'utf8'))], null, new InMemoryExecutionContext());
-        console.log(sourceFile);
+        const [sourceFile] = parser.parseInputs(
+            [new ParserInput('foo.ts', null, true, () => Buffer.from('1', 'utf8'))],
+            null,
+            new InMemoryExecutionContext()
+        ) as Iterable<JS.CompilationUnit>;
+        expect(sourceFile).toBeDefined();
     });
 
     test('parseStrings', () => {
-        const parser = JavaScriptParser.builder().build();
         const [sourceFile] = parser.parseStrings(`
         const c = 1;
         /* c1*/  /*c2 */const d = 1;`) as Iterable<JS.CompilationUnit>;
+        expect(sourceFile).toBeDefined();
+    });
+});
+
+describe('LST mapping', () => {
+    const parser = JavaScriptParser.builder().build();
+
+    test('parseInputs', () => {
+        const sourceFile = javaScript('1');
+        expect(sourceFile).toBeDefined();
+        expect(sourceFile.statements).toHaveLength(1);
+        expect(sourceFile.statements[0]).toBeInstanceOf(JS.ExpressionStatement);
+    });
+
+    test('parseStrings', () => {
+        //language=typescript
+        const sourceFile = javaScript(`
+            const c = 1;
+            /* c1*/  /*c2 */
+            const d = 1;
+        `);
         expect(sourceFile).toBeDefined();
         expect(sourceFile.statements).toHaveLength(2);
         sourceFile.statements.forEach(statement => {
             expect(statement).toBeInstanceOf(J.Unknown);
         })
     });
+
+    function javaScript(source: string): JS.CompilationUnit {
+        const [sourceFile] = parser.parseStrings(dedent(source)) as Iterable<JS.CompilationUnit>;
+        return sourceFile;
+    }
 });
