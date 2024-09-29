12a8749c01f668316e5e01f9ef3c8327317a1cd7
Knut Wannheden <knut@moderne.io>
2024-09-29 06:06:18 +0200

    Remove old Javet-based parser (#119)
    The Javet jars are very large (50MB each), because they bundle V8 and NodeJS. As a result the `rewrite-javascript` is over 100MB when counting its dependencies.

--- js/src/codegen/generateInterfaces.ts
+++
@@ -1,9 +0,0 @@
-async function main() {}
-
-function getTypeScriptTypeScriptProject() {}
-
-if (require.main === module) {
-    main()
-        .then()
-        .catch((err) => console.error(err));
-}
