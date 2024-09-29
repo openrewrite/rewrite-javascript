cf21a3a34a60b7fd1e59a33e5a0a98fc31233057
Knut Wannheden <knut@moderne.io>
2024-09-29 06:08:06 +0200

    Revert change to `ParserTest.java`

--- src/test/java/org/openrewrite/javascript/tree/ParserTest.java
+++ src/test/java/org/openrewrite/javascript/tree/ParserTest.java
@@ -15,24 +15,9 @@
  */
 package org.openrewrite.javascript.tree;
 
-import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
 import org.junit.jupiter.api.Test;
-import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.Issue;
-import org.openrewrite.SourceFile;
-import org.openrewrite.remote.ReceiverContext;
-import org.openrewrite.remote.RemotingContext;
-import org.openrewrite.remote.RemotingMessenger;
-import org.openrewrite.remote.java.RemotingClient;
 import org.openrewrite.test.RewriteTest;
-
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
 
 import static org.openrewrite.javascript.Assertions.javaScript;
 
@@ -46,36 +31,5 @@ class ParserTest implements RewriteTest {
           javaScript("class B {}", s -> s.path("B.js")),
           javaScript("class C {}", s -> s.path("C.js"))
         );
-    }
-
-    @Test
-    void project() {
-        RemotingContext context = new RemotingContext(RewriteTest.class.getClassLoader(), false);
-        Map<String, Supplier<RemotingMessenger.RequestHandler<?>>> handlers = Map.of();
-        RemotingClient client = new RemotingClient(() -> {
-            try {
-                return new Socket(InetAddress.getLoopbackAddress(), 54323);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }, context, new RemotingMessenger((CBORFactory) context.objectMapper().getFactory(), handlers, (m) -> new InMemoryExecutionContext()));
-
-        List<SourceFile> sourceFileStream = client.runUsingSocket((socket, messenger) -> messenger.sendRequest(generator -> {
-            generator.writeString("parse-project-sources");
-            generator.writeString("/Users/knut/git/emilkowalski/vaul");
-        }, parser -> {
-            parser.nextToken();
-            int count = parser.getIntValue();
-
-            ReceiverContext receiverContext = new ReceiverContext(context.newReceiver(parser), context);
-            List<SourceFile> sourceFiles = new ArrayList<>(count);
-            for (int i = 0; i < count; i++) {
-                SourceFile sourceFile = receiverContext.receiveTree(null);
-                System.out.println(sourceFile.getSourcePath());
-                sourceFiles.add(sourceFile);
-            }
-            return sourceFiles.stream();
-        }, socket)).toList();
-        System.out.println(sourceFileStream);
     }
 }
