0002b4672c4afb21d1784e21abbe54b74fd0928e
OlegDokuka <oleh@moderne.io>
2024-10-17 13:21:29 +0300

    remove runtime dependency
    Signed-off-by: OlegDokuka <oleh@moderne.io>

--- rewrite-javascript/build.gradle.kts
+++ rewrite-javascript/build.gradle.kts
@@ -13,8 +13,6 @@ dependencies {
         exclude(group = "org.openrewrite", module = "rewrite-javascript")
     }
 
-    runtimeOnly(project(":rewrite-javascript-remote"))
-
     compileOnly("org.assertj:assertj-core:latest.release")
     testImplementation("org.assertj:assertj-core:latest.release")
     testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
