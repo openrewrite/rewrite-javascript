126eb20a2103621965fc96a544e384e219535e98
Oleh Dokuka <oleh.dokuka@icloud.com>
2024-12-17 01:10:29 +0200

    add jar to embed and bundle JS server as a jar

---
+++ rewrite-javascript-remote-server/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("org.openrewrite.build.language-library")
+}
+
+tasks.compileJava {
+    options.release = 8
+}
+
+// We don't care about publishing javadocs anywhere, so don't waste time building them
+tasks.withType<Javadoc>().configureEach {
+    enabled = false
+}
+
+tasks.named<Jar>("sourcesJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("javadocJar") {
+    enabled = false
+}
+
+val emptySourceJar = tasks.create<Jar>("emptySourceJar") {
+    file("README.md")
+    archiveClassifier.set("sources")
+}
+
+val emptyJavadocJar = tasks.create<Jar>("emptyJavadocJar") {
+    file("README.md")
+    archiveClassifier.set("javadoc")
+}
